9e1f79737d680cb48c5578355d4bf2a48b63248e
Mauricio García <m4ur1c1o.gg@gmail.com>
2016-12-01 22:22:40 -0600

    Add all views for static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,28 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def cuento
+
+  end
+
+  def novela
+
+  end
+
+  def mito
+
+  end
+
+  def epopeya
+
+  end
+
+  def fabula
+
+  end
+
+  def bibliografia
+
+  end
 end
